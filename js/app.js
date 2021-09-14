854e7dc45f5016ec94a4cbe8e3bf1dc847b17b33
Kauã Cavalcante <kaua.ferreiracavalcante@gmail.com>
2021-09-13 22:38:44 -0300

    :tada: initial commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+$('header .menu a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href');
+        targetOffset = $(id).offset().top,
+        menuHeight = $('header').innerHeight();
+
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);
+});
+
+// Debounce Lodash
+debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+
+(function() {
+var $target = $('.animation'),
+    animationClass = 'start-animation',
+    offset = $(window).height() + 3/4;
+
+function animeScroll() {
+    var documentTop = $(document).scrollTop();
+    
+    $target.each(function() {
+        var itemTop = $(this).offset().top;
+        if(documentTop > itemTop - offset) {
+            $(this).addClass(animationClass);
+        } else {
+            $(this).removeClass(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+$(document).scroll(debounce(function(){
+    animeScroll();
+}, 200));
+
+}());
