4701e17cf5ed251bc76520937e14733f49a1b5bb
Kauã Cavalcante <kaua.ferreiracavalcante@gmail.com>
2021-09-21 00:05:04 -0300

    responsiveness

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+let show = true;
+
+const menuSection = document.querySelector(".menu__section");
+const menuToggle = menuSection.querySelector(".menu__toggle");
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
