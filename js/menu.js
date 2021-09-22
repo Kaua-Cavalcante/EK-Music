9416f176c8d647d26ed21bb667fbd8e35a880460
Kauã Cavalcante <kaua.ferreiracavalcante@gmail.com>
2021-09-22 18:38:31 -0300

    menu fixed

--- js/menu.js
+++ js/menu.js
@@ -5,8 +5,18 @@ const menuToggle = menuSection.querySelector(".menu__toggle");
 
 menuToggle.addEventListener("click", () => {
 
-    document.body.style.overflow = show ? "hidden" : "initial"
+    document.body.style.overflow = show ? "hidden" : "initial";
 
-    menuSection.classList.toggle("on", show)
+    menuSection.classList.toggle("on", show);
     show = !show;
-})
+});
+
+function closeMenu() {
+    if(show = true) {
+
+        document.body.style.overflow = !show ? "hidden" : "initial";
+
+        menuSection.classList.remove('on');
+        show = true;
+    }
+}
